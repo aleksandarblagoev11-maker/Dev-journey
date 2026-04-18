b1617295315e4c93274c3b50bc2dc871f44cca97
aleksandarblagoev11-maker <aleksandarblagoev11@gmail.com>
2026-04-18 10:58:10 +0100

    My first Python program

---
+++ Hello.py
@@ -0,0 +1,6 @@
+name = input("What is your Name?: ")
+Job = "AI Engineer"
+Years_experiance = 0
+print(f"Hello, my name is {name}")
+print(f"i am a {Job}")
+print(f"Yearts of Experieance: {Years_experiance}")
